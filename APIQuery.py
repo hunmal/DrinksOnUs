b241e9b8b77831908c528a1d6179c5bb2f0b1f0e
hunmal <64437916+hunmal@users.noreply.github.com>
2021-12-06 21:17:07 -0500

    non-alc json, updated searchByIng(), comments added

--- APIQuery.py
+++ APIQuery.py
@@ -12,34 +12,71 @@ TO USE:
 """
 
 class APIQuery():
-
     def __init__(self):
         # Key to acess any API endpoint
         self.key = "https://www.thecocktaildb.com/api/json/v2/9973533/"
         # Multiple ingredient API endpoint
-        self.ingKey = self.key + "filter.php?i="
+        self.ingKey = self.key + "filter.php?i="    # Filter by alc ingredients
+        self.naKey = self.key + "filter.php?a=Non_Alcoholic"    # Filter by non-alc 
+        self.lingKey = self.key + "lookup.php?i="     # look up receipe for drink by ID
     
     def searchWorker(self, q):
         # q is an API call
         url = q
         response = urlopen(url) # open URL (API call)
-        qResponse = json.loads(response.read()) # Read JSON response to call and load 
+        qResponse = json.load(response) # Read JSON response to call and load 
         print(qResponse)
         return qResponse
 
-
+    # Only constraints are ingredients
     def searchByIngredients(self, xs):
-        # masterSearch is multipe ingredient search API endpoint concat with list of ingredients xs
-        masterSearch = self.ingKey + ",".join(xs)
+        """
+            Takes list of ingredients
+            Only constraints are ingredients
+            Returns a tuple of a list of dictionaries where each dictionary is full drink profile and number of
+                ingredients in the drink
+        """
+        # ingSearch is multiple ingredient search API endpoint concat with list of ingredients xs
+        ingSearch = self.ingKey + ",".join(xs)
         print("\n")
-        print(masterSearch)
-        
-        return self.searchWorker(masterSearch)
+        print(ingSearch)
+        workerRes = self.searchWorker(ingSearch)
 
+        resDrinkIds = [x["idDrink"] for x in workerRes["drinks"]]   # Get ids of all drinks returned
+        # lookup by id drinks to get their full profile
+        drinksAndInstr = [self.searchWorker(self.lingKey + x)["drinks"][0] for x in resDrinkIds]
+        ingCount = self.ingFinder(drinksAndInstr) # find number of ingredients for each drink
+
+        return (drinksAndInstr, ingCount)
+
+    # Not in active use, used once to get all non alc data
+    # def scrapeNonAlc(self):
+    #     ingSearch = self.naKey
+    #     print("\n")
+    #     print(ingSearch)
+    #     workerRes = self.searchWorker(ingSearch)
+
+    #     resDrinkIds = [x["idDrink"] for x in workerRes["drinks"]]   # Get ids of all drinks returned
+    #     # lookup by id drinks to get their full profile
+    #     drinksAndInstr = [self.searchWorker(self.lingKey + x)["drinks"][0] for x in resDrinkIds]
+
+    #     outfile = open('nonalc.json', 'w')
+    #     json.dump(drinksAndInstr, outfile)
     
+    def ingFinder(self, drinks, xs):
+        ingCounter = []
+        for d in drinks:
+            ingCount = 0
+            while True:
+                if d["strMeasure" + ingCount] == "null":
+                    break
+                ingCount += 1
+            ingCounter.append(ingCount)
+        return drinks
+             
 
 q = APIQuery()
-q.searchByIngredients(['Gin', 'Vodka'])
+#q.searchByIngredients(['Gin', 'Lime'])
 
     
 
