53d759fb4e5d44f2b8652a879cd08bac03f46e52
hunmal <64437916+hunmal@users.noreply.github.com>
2021-12-14 10:49:59 -0500

    comments added; server.py ready for submit"

--- server.py
+++ server.py
@@ -2,42 +2,43 @@ from flask import Flask, render_template, request, Response
 from flask.helpers import make_response
 from werkzeug.wrappers import response
 from APIQuery import APIQuery
-import json
 import os
 
+# app is Flask server object
 app = Flask(__name__)
-#app.static_folder='DrinksOnUs/static'
+# static_folder_root is path to static folder in project directory
 static_folder_root = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-# app.register_routes_to_resources(static_folder_root)
 
+# When GET request for landing page is made
 @app.route('/')
 def hello():
-    return render_template("/index.html")
+    return render_template("/index.html") # Render HTML template index.html as response to GET
 
+# When GET request for mocktail page is made
 @app.route('/mocktail/')
 def mocktail():
     return render_template("/mocktail.html")
 
+# When POST request for mocktail search is made
 @app.route('/mocktail/search',methods=['POST'])
 def mocktailSearch():
-    data = list(dict(request.form).keys())
-    q = APIQuery()
-    results = q.nonAlcSearchByIngredients(data)
-    #render_template("/mocktail.html")
+    data = list(dict(request.form).keys()) # Get data from HTML form
+    q = APIQuery() # Init instance of APIQuery object
+    results = q.nonAlcSearchByIngredients(data) # Query API through APIQuery member func
     response  = make_response(render_template("/mocktail.html", results=results))
-    return response
-    return str(results)
+    return response # Make and return a response object to be accessed in HTML
 
+# When GET request for cocktial page is made
 @app.route('/cocktail/')
 def cocktail():
     return render_template("/cocktail.html")
     
-
+# When POST request for cocktial search is made
 @app.route('/cocktail/search/')
 def cocktailSearch():
-    data = list(dict(request.form).keys())#['subSearchMock']
-    q = APIQuery()
-    results = q.searchByIngredients(data)
+    data = list(dict(request.form).keys()) # Get data from HTML form
+    q = APIQuery() # Init instance of APIQuery object
+    results = q.searchByIngredients(data) # Query API through APIQuery member func
     response  = make_response(render_template("/cocktail.html", results=results))
-    return response
+    return response # Make and return a response object to be accessed in HTML
     
